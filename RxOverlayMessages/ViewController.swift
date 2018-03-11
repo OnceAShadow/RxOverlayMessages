85aa272a3948ffc23a6e802cbea7a7a5cb0d8783
OnceAShadow <fred.sevillano@gmail.com>
2018-03-10 21:59:11 -0500

    Initial Commit

---
+++ RxOverlayMessages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxOverlayMessages
+//
+//  Created by iOS MAC on 3/10/18.
+//  Copyright © 2018 iOS MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
