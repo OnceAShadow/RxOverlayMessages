3a852738eb55de01ca8f1fb8860e4cacb43b7861
OnceAShadow <fred.sevillano@gmail.com>
2018-03-11 00:41:35 -0500

    Folders and Clean up

--- RxOverlayMessages/ViewController.swift
+++ RxOverlayMessages/ViewController.swift
@@ -1,8 +1,8 @@
 //  ViewController.swift
 //  RxOverlayMessages
 //
-//  Created by iOS MAC on 3/10/18.
-//  Copyright © 2018 iOS MAC. All rights reserved.
+//  Created by Fred Sevillano on 3/10/18.
+//  Copyright © 2018 Fred Sevillano All rights reserved.
 
 import UIKit
 
@@ -10,12 +10,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
